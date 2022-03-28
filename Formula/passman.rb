ff8931a88ffcc5adec6cc6280c91186316cf0156
harun <harun@goodjobgames.com>
2022-03-28 17:50:28 +0300

    passman v1.0.1

--- Formula/passman.rb
+++ Formula/passman.rb
@@ -2,27 +2,27 @@ require 'rbconfig'
 class Passman < Formula
   desc "A simple local password manager for daily use"
   homepage "https://github.com/harunsasmaz/passman"
-  version "1.0.0"
+  version "1.0.1"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/harunsasmaz/passman/releases/download/v1.0.0/passman_1.0.0_darwin_x86_64.tar.gz"
-      sha256 "328cbb430450a749f92c9f7460c463f5bd367daf4d46f2f63763d0b3258415e6"
+      url "https://github.com/harunsasmaz/passman/releases/download/v1.0.1/passman_1.0.1_darwin_x86_64.tar.gz"
+      sha256 "0dea51eeca9df4cc1604d14bc01a7c90cf2087dbe0ae3e930bd73975955ab6bd"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/harunsasmaz/passman/releases/download/v1.0.0/passman_1.0.0_darwin_arm64.tar.gz"
-      sha256 "9d5d9fa24ce32910e9b9e8bdd564240766bc532bc85e7d457fe948d8ccd35a6e"
+      url "https://github.com/harunsasmaz/passman/releases/download/v1.0.1/passman_1.0.1_darwin_arm64.tar.gz"
+      sha256 "0013b0dd4d3542774f6660271779ee5c3835141accb5174ea56a3517f1d2a0e2"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/harunsasmaz/passman/releases/download/v1.0.0/passman_1.0.0_linux_x86_64.tar.gz"
-      sha256 "23e5f153813d9d2182eb7245622d09d372d4f61645325e7257f0f552ddb968ea"
+      url "https://github.com/harunsasmaz/passman/releases/download/v1.0.1/passman_1.0.1_linux_x86_64.tar.gz"
+      sha256 "1b4bafa419b724816cdf0b45cfb592e28694162f19e1e33a3eca1bf436f788ff"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/harunsasmaz/passman/releases/download/v1.0.0/passman_1.0.0_linux_arm64.tar.gz"
-      sha256 "9fbd77a7d319c274244977e9f898940c94388ed549bd2a77a00ce113153215bc"
+      url "https://github.com/harunsasmaz/passman/releases/download/v1.0.1/passman_1.0.1_linux_arm64.tar.gz"
+      sha256 "601507ed41d6f6e0bb1757602a9486a6dac52da30cc5ebd7dcc6d8e72ea7dcc2"
     end
   end
 
